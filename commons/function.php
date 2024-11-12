4857b7e5d36f6ceca6f397f23ece610e3b6208c0
Nguyễn Đình Bổng <bong19062005@gmail.com>
2024-11-12 10:06:51 +0700

    Update function.php

--- commons/function.php
+++ commons/function.php
@@ -1,14 +1,18 @@
 <?php
-    function connectDB(){
-        $host="mysql:host=localhost;dbname=duan1;charset=utf8";
-        $user="root";
-        $pass="";
-        try {
-            $conn=new PDO($host,$user,$pass);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $conn;
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
+
+// Hàm kết nối tới CSDL
+function connectDB() {
+    $host = "localhost";
+    $dbname = "duan1";
+    $username = "root";
+    $password = "";
+    $port = "3306";
+    
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$dbname;port=$port;charset=utf8", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (PDOException $e) {
+        echo "Lỗi kết nối CSDL: " . $e->getMessage();
     }
-?>
+}
