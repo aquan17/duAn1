cc32345bf0e3d541c792b3a637a4359a099efc84
Nguyễn Đình Bổng <bong19062005@gmail.com>
2024-11-13 13:54:10 +0700

    Update function.php

--- commons/function.php
+++ commons/function.php
@@ -1,18 +1,22 @@
 <?php
-
-// Hàm kết nối tới CSDL
+// Hàm connection: kết nối đến CSDL
 function connectDB() {
     $host = "localhost";
-    $dbname = "duan1";
+    $dbname = "duan1"; 
     $username = "root";
     $password = "";
     $port = "3306";
-    
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname;port=$port;charset=utf8", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
         return $conn;
-    } catch (PDOException $e) {
-        echo "Lỗi kết nối CSDL: " . $e->getMessage();
+    } catch(PDOException $e) {
+        echo "Lỗi kết nối CSDL: ". $e->getMessage();
     }
+}
+
+// Hàm view: hàm dùng để gọi trang hiển thị
+function view($view, $data = []) {
+    extract($data);
+    include_once "views/$view.php";
 }
