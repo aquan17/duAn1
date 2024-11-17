6b95d71eb633dd3d16ecbf886f2e73730a38d492
Nguyễn Đình Bổng <bong19062005@gmail.com>
2024-11-17 18:06:03 +0700

    update thêm về admin

--- admin/views/category/list.php
+++ admin/views/category/list.php
@@ -1,6 +1,109 @@
-<h2>Danh sách danh mục</h2>
-<a href="index.php?ctl=category-add">Thêm danh mục</a>
-<table border="1">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Quản Trị</title>
+    <!-- Boxicons -->
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <!-- My CSS -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        table th {
+            padding: 10px 20px;
+            color: darkgreen;
+            font-weight: bolder;
+        }
+
+        table {
+            background-color: gainsboro;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        table td {
+            padding: 10px 15px;
+            text-align: center;
+        }
+
+        table button {
+            padding: 5px 10px;
+            border-radius: 5px;
+            border: 1px #ccc solid;
+            font-size: 0.7vw;
+        }
+
+        table img {
+            width: 5vw;
+            height: 5vw;
+        }
+
+        .checkborder {
+            border: 1px solid #ccc;
+            margin-top: 15px;
+            padding: 5px 10px;
+            border-radius: 5px;
+        }
+
+
+        input[type="text"],
+        textarea,
+        select,
+        input[type=date],
+        input[type=file],
+        input[type=password],
+        input[type=email],
+        input[type="search"],
+        input[type="number"] {
+            width: 100%;
+            padding: 5px 15px;
+            border-radius: 5px;
+            font-size: 1vw;
+            border: 1px #ccc solid;
+        }
+
+
+        input:disabled {
+            background-color: #eee;
+            border-color: #ccc;
+        }
+
+        input[type="submit"],
+        input[type="reset"],
+        input[type="button"] {
+            padding: 10px 10px;
+            border-radius: 5px;
+            border: 1px #ccc solid;
+            font-size: 0.7vw;
+            margin: 20px 0px;
+        }
+
+        input[type="submit"]:hover,
+        input[type="reset"]:hover,
+        input[type="button"]:hover,
+        button:hover {
+            background-color: aqua;
+        }
+    </style>
+</head>
+
+<body>
+    <?php require_once 'views/components/navbar.php'; ?>
+    <br>
+    <section id="content" style="margin-left: 10px;">
+    <h2>Danh sách danh mục</h2>
+<a href="index.php?ctl=category-add"><button class="btn btn-primary">Thêm Danh Mục</button></a>
+<table class="table table-striped table-bordered">
     <tr>
         <th>ID</th>
         <th>Tên danh mục</th>
@@ -11,9 +114,15 @@
         <td><?= $category['category_id'] ?></td>
         <td><?= $category['category_name'] ?></td>
         <td>
-            <a href="index.php?ctl=category-edit&id=<?= $category['category_id'] ?>">Sửa</a>
-            <a href="index.php?ctl=category-delete&id=<?= $category['category_id'] ?>" onclick="return confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+            <a href="index.php?ctl=category-edit&id=<?= $category['category_id'] ?>"><button class="btn btn-warning">Sửa</button></a>
+            <a href="index.php?ctl=category-delete&id=<?= $category['category_id'] ?>" onclick="return confirm('Bạn có chắc chắn muốn xóa?')"><button class="btn btn-danger">Xóa</button></a>
         </td>
     </tr>
     <?php endforeach; ?>
 </table>
+
+
+    </section>
+</body>
+
+</html>
--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -3,12 +3,151 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trang quản trị</title>
+    <title>Document</title>
+     <!-- Boxicons -->
+     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <!-- My CSS -->
+    <link rel="stylesheet" href="../assets/css/a.css">
 </head>
 <body>
-<h1>TRANG QUẢN TRỊ WEBSITE</h1>
-<a href=""><Button>Quản trị ...</Button></a>
-<a href="index.php?ctl=category-list">Danh Mục</a>
-<a href="index.php?ctl=product-list">Sản Phẩm</a>
+    <?php require_once 'views/components/navbar.php'; ?>
+
+    <section id="content">
+    <main>
+            <div class="head-title">
+                <div class="left">
+                    <h1>Dashboard</h1>
+                    <ul class="breadcrumb">
+                        <li>
+                            <a href="#">Dashboard</a>
+                        </li>
+                        <li><i class='bx bx-chevron-right'></i></li>
+                        <li>
+                            <a class="active" href="#">Home</a>
+                        </li>
+                    </ul>
+                </div>
+                <a href="#" class="btn-download">
+                    <i class='bx bxs-cloud-download'></i>
+                    <span class="text">Download PDF</span>
+                </a>
+            </div>
+
+            <ul class="box-info">
+                <li>
+                    <i class='bx bxs-calendar-check'></i>
+                    <span class="text">
+                        <h3>1020</h3>
+                        <p>New Order</p>
+                    </span>
+                </li>
+                <li>
+                    <i class='bx bxs-group'></i>
+                    <span class="text">
+                        <h3>2834</h3>
+                        <p>Visitors</p>
+                    </span>
+                </li>
+                <li>
+                    <i class='bx bxs-dollar-circle'></i>
+                    <span class="text">
+                        <h3>$2543</h3>
+                        <p>Total Sales</p>
+                    </span>
+                </li>
+            </ul>
+
+
+            <div class="table-data">
+                <div class="order">
+                    <div class="head">
+                        <h3>Recent Orders</h3>
+                        <i class='bx bx-search'></i>
+                        <i class='bx bx-filter'></i>
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>User</th>
+                                <th>Date Order</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img src="img/people.png">
+                                    <p>Nguyễn Anh Quân</p>
+                                </td>
+                                <td>17-10-2004</td>
+                                <td><span class="status completed">Completed</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="img/people.png">
+                                    <p>Lăng Minh Đăng</p>
+                                </td>
+                                <td>01-4-2023</td>
+                                <td><span class="status pending">Pending</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="img/people.png">
+                                    <p>Nguyễn Hữu Đan</p>
+                                </td>
+                                <td>07-2-2022</td>
+                                <td><span class="status process">Process</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="img/people.png">
+                                    <p>Nguyễn Đức Phú</p>
+                                </td>
+                                <td>27-5-2024</td>
+                                <td><span class="status pending">Pending</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="img/people.png">
+                                    <p>J97</p>
+                                </td>
+                                <td>14-8-2022</td>
+                                <td><span class="status completed">Completed</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="todo">
+                    <div class="head">
+                        <h3>Todos</h3>
+                        <i class='bx bx-plus'></i>
+                        <i class='bx bx-filter'></i>
+                    </div>
+                    <ul class="todo-list">
+                        <li class="completed">
+                            <p>Todo List</p>
+                            <i class='bx bx-dots-vertical-rounded'></i>
+                        </li>
+                        <li class="completed">
+                            <p>Todo List</p>
+                            <i class='bx bx-dots-vertical-rounded'></i>
+                        </li>
+                        <li class="not-completed">
+                            <p>Todo List</p>
+                            <i class='bx bx-dots-vertical-rounded'></i>
+                        </li>
+                        <li class="completed">
+                            <p>Todo List</p>
+                            <i class='bx bx-dots-vertical-rounded'></i>
+                        </li>
+                        <li class="not-completed">
+                            <p>Todo List</p>
+                            <i class='bx bx-dots-vertical-rounded'></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+        </section>
 </body>
 </html>
