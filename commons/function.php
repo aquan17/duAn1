8c5e0adbc350fc80a09fd2e4b1684b892197fe76
Nguyễn Đình Bổng <bong19062005@gmail.com>
2024-12-04 15:55:19 +0700

    Update

--- commons/function.php
+++ commons/function.php
@@ -2,7 +2,7 @@
 // Hàm connection: kết nối đến CSDL
 function connectDB() {
     $host = "localhost";
-    $dbname = "duan-1"; 
+    $dbname = "duan1"; 
     $username = "root";
     $password = "";
     $port = "3306";
